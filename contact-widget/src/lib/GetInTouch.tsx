8eccf0cf20779e8ae9b52024558547706d4dd915
Kalyan Annamalai <kalyan.shelf@gmail.com>
2023-03-05 09:19:20 +1100

    draft app

---
+++ contact-widget/src/lib/GetInTouch.tsx
@@ -0,0 +1,14 @@
+export const GetInTouch = () => {
+  return (
+    <div class='container'>
+      <div class='cta-inner section-inner'>
+        <h3 class='section-title mt-0'>Still not convinced on buying?</h3>
+        <div class='cta-cta'>
+          <a class='button button-primary button-wide-mobile' href='#'>
+            Get in touch
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
